467defdb9ac71b724f81d43a3ebf59601b904c1e
Allen Royston <allen.acr@gmail.com>
2016-08-07 17:53:37 -0700

    Add files via upload

---
+++ ng2parallax.js
@@ -0,0 +1,10 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+var parallax_directive_1 = require("./ng2-parallax-directive/parallax.directive");
+__export(require("./ng2-parallax-directive/parallax.directive"));
+exports.ng2Parallax = [
+    parallax_directive_1.ng2parallax,
+];
+//# sourceMappingURL=ng2parallax.js.map
---
+++ ng2parallax.ts
@@ -0,0 +1,7 @@
+import { ng2parallax } from "./ng2-parallax-directive/parallax.directive";
+
+export * from "./ng2-parallax-directive/parallax.directive";
+
+export let ng2Parallax: Array<any> = [
+  ng2parallax,
+]
